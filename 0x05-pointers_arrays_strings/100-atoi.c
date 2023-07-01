4f2bd5b1b44a3fc74671b885a26416fa36106d7f
Abdelaziz Sakhi <abdesakhi15@gmail.com>
2023-06-30 18:19:45 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _atoi - Convert string to an integer.
+ * _atoi - Convert a string to an integer.
  * @s: The pointer to convert
+ *
  * Return: A integer
  */
 int _atoi(char *s)
@@ -14,21 +15,26 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
-	if (s[c] == 45)
-	{
-	min *= -1;
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
 	}
-	while (s[c] >= 48 && s[c] <= 57)
-	{
-	isi = 1:
-	ni = (ni * 10) + (s[c] - '0');
-	c++
-	}
-	if (isi == 1)
-	break;
-	}
-	c++;
-	}
+
 	ni *= min;
 	return (ni);
 }
