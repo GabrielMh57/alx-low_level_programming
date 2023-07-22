5eb1e66591c386f4575ca116846b146bc9224659
Abdelaziz Sakhi <abdesakhi15@gmail.com>
2023-07-21 19:51:01 -0700

    b

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,17 +1,10 @@
-#ifndef CALC
-#define CALC
-#include <stdlib.h>
-#include<string.h>
-#include <stdarg.h>
-#include <stdio.h>
-
+#ifndef CALC_H
+#define CALC_H
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
-
 typedef struct op
 {
 	char *op;
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,18 +1,14 @@
 #include "3-calc.h"
+#include <stdlib.h>
 /**
- * get_op_func - function that selects the correct function to perform the
- *               operation asked by the user.
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
  *
- * @s: This is the input operator
- *
- * Return: This function returns a pointer to the function that corresponds to
- *         the operator given as a parameter. Example: get_op_func("+")
- *         should return a pointer to the function op_add"")
- *         If s does not match any of the 5 expected operators
- *         (+, -, *, /, %),return NULL)
- *
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
-int (*get_op_func(char *s))(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
@@ -20,17 +16,13 @@ int (*get_op_func(char *s))(int a, int b)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
+		{NULL, NULL},
 	};
+
 	int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if (*ops[i].op == *s)
-		{
-			return (ops[i].f);
-		}
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+
+	return (ops[i].f);
 }
